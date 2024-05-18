c3712d20931ecc008acb90910aab47dc670e7b65
석찬 윤 <ysc0731@ajou.ac.kr>
2024-05-19 00:09:16 +0900

    Feat: Add FCM onmessage

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,84 @@
 // firebase-messaging-sw.js
-import { firebaseApp } from "../src/fcm/firebase";
 import {
+  initializeApp,
   getMessaging,
   onMessage,
   onBackgroundMessage,
 } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+
+onMessage(messaging, (e) => {
+  console.log("Message received. ", payload);
+  if (!e.data || !e.data.json()) {
+    console.error("Push event does not contain valid JSON data.");
+    return;
+  }
+
+  const resultData = e.data.json().notification;
+  const resultURL = e.data.json().data.click_action;
+  self.addEventListener("notificationclick", function (event) {
+    event.waitUntil(clients.openWindow(resultURL));
+    event.notification.close();
+  });
+
+  if (!resultData || !resultData.title || !resultData.body) {
+    console.error("Notification data is incomplete.");
+    return;
+  }
+
+  const notificationTitle = resultData.title;
+  const notificationOptions = {
+    body: resultData.body,
+    icon: resultData.image,
+    tag: resultData.tag,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+onBackgroundMessage(messaging, (e) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  if (!e.data || !e.data.json()) {
+    console.error("Push event does not contain valid JSON data.");
+    return;
+  }
+
+  const resultData = e.data.json().notification;
+  const resultURL = e.data.json().data.click_action;
+  self.addEventListener("notificationclick", function (event) {
+    event.waitUntil(clients.openWindow(resultURL));
+    event.notification.close();
+  });
+
+  if (!resultData || !resultData.title || !resultData.body) {
+    console.error("Notification data is incomplete.");
+    return;
+  }
+
+  const notificationTitle = resultData.title;
+  const notificationOptions = {
+    body: resultData.body,
+    icon: resultData.image,
+    tag: resultData.tag,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
 self.addEventListener("install", function (e) {
   console.log("fcm sw install..");
@@ -38,27 +112,6 @@ self.addEventListener("push", function (e) {
     body: resultData.body,
     icon: resultData.image,
     tag: resultData.tag,
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-const messaging = getMessaging(firebaseApp);
-
-onMessage(messaging, (payload) => {
-  console.log("Message received. ", payload);
-});
-
-onBackgroundMessage(messaging, (payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
-  const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
