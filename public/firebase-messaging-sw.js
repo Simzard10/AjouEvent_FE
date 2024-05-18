577f97f964a345031f60d0f22051ab565f58bf3d
석찬 윤 <ysc0731@ajou.ac.kr>
2024-05-19 00:43:38 +0900

    Feat: fcm service worker 코드개선

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-// firebase-messaging-sw.js
 import { initializeApp } from "firebase/app";
 import {
   getMessaging,
@@ -19,19 +18,9 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
-onMessage(messaging, (e) => {
-  console.log("Message received. ", payload);
-  if (!e.data || !e.data.json()) {
-    console.error("Push event does not contain valid JSON data.");
-    return;
-  }
-
-  const resultData = e.data.json().notification;
-  const resultURL = e.data.json().data.click_action;
-  self.addEventListener("notificationclick", function (event) {
-    event.waitUntil(clients.openWindow(resultURL));
-    event.notification.close();
-  });
+const showNotification = (data) => {
+  const resultData = data.notification;
+  const resultURL = data.data.click_action;
 
   if (!resultData || !resultData.title || !resultData.body) {
     console.error("Notification data is incomplete.");
@@ -46,73 +35,57 @@ onMessage(messaging, (e) => {
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
+
+  self.addEventListener("notificationclick", function (event) {
+    event.notification.close(); // Close the notification
+
+    event.waitUntil(
+      clients
+        .matchAll({ type: "window", includeUncontrolled: true })
+        .then(function (clientList) {
+          for (let i = 0; i < clientList.length; i++) {
+            const client = clientList[i];
+            // Check if the URL matches
+            if (client.url === resultURL && "focus" in client) {
+              return client.focus();
+            }
+          }
+          // If no matching client is found, open a new window
+          if (clients.openWindow) {
+            return clients.openWindow(resultURL);
+          }
+        })
+    );
+  });
+};
+
+const handlePushEvent = (e) => {
+  console.log("Push event received. ", e);
+  if (!e.data || !e.data.json()) {
+    console.error("Push event does not contain valid JSON data.");
+    return;
+  }
+  const data = e.data.json();
+  showNotification(data);
+};
+
+onMessage(messaging, (e) => {
+  console.log("Foreground message received. ", e);
+  handlePushEvent(e);
 });
 
 onBackgroundMessage(messaging, (e) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  if (!e.data || !e.data.json()) {
-    console.error("Push event does not contain valid JSON data.");
-    return;
-  }
-
-  const resultData = e.data.json().notification;
-  const resultURL = e.data.json().data.click_action;
-  self.addEventListener("notificationclick", function (event) {
-    event.waitUntil(clients.openWindow(resultURL));
-    event.notification.close();
-  });
-
-  if (!resultData || !resultData.title || !resultData.body) {
-    console.error("Notification data is incomplete.");
-    return;
-  }
-
-  const notificationTitle = resultData.title;
-  const notificationOptions = {
-    body: resultData.body,
-    icon: resultData.image,
-    tag: resultData.tag,
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log("Background message received. ", e);
+  handlePushEvent(e);
 });
 
-self.addEventListener("install", function (e) {
-  console.log("fcm sw install..");
+self.addEventListener("push", handlePushEvent);
+
+self.addEventListener("install", (e) => {
+  console.log("Service worker installed.");
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function (e) {
-  console.log("fcm sw activate..");
-});
-
-self.addEventListener("push", function (e) {
-  if (!e.data || !e.data.json()) {
-    console.error("Push event does not contain valid JSON data.");
-    return;
-  }
-
-  const resultData = e.data.json().notification;
-  const resultURL = e.data.json().data.click_action;
-  self.addEventListener("notificationclick", function (event) {
-    event.waitUntil(clients.openWindow(resultURL));
-    event.notification.close();
-  });
-
-  if (!resultData || !resultData.title || !resultData.body) {
-    console.error("Notification data is incomplete.");
-    return;
-  }
-
-  const notificationTitle = resultData.title;
-  const notificationOptions = {
-    body: resultData.body,
-    icon: resultData.image,
-    tag: resultData.tag,
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+self.addEventListener("activate", (e) => {
+  console.log("Service worker activated.");
 });
