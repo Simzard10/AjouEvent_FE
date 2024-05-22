21984e2c803671f1ba58490957db848bbf85bc88
석찬 윤 <ysc0731@ajou.ac.kr>
2024-05-22 23:24:12 +0900

    Add: location.reload();

--- src/fcm/GetUserPermission.jsx
+++ src/fcm/GetUserPermission.jsx
@@ -1,4 +1,5 @@
 import GetFCMToken from "./GetFCMToken";
+import { registerServiceWorker } from "../serviceWorkerRegistration";
 
 const GetUserPermission = async () => {
   try {
@@ -6,7 +7,9 @@ const GetUserPermission = async () => {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
       console.log("Notification permission granted. Ready to send token...");
+      await registerServiceWorker();
       await GetFCMToken();
+      location.reload();
     } else {
       console.log(
         "Notification permission not granted. Requesting permission..."
--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -5,8 +5,7 @@ import EventMain from "../events/EventMain";
 import BottomNavbar from "../components/BottomNavbar";
 import SearchDropBox from "../events/SearchDropBox";
 import SearchBar from "../components/SearchBar";
-import GetFCMToken from "../fcm/GetFCMToken";
-import { registerServiceWorker } from "../serviceWorkerRegistration";
+import GetUserPermission from "../fcm/GetUserPermission";
 
 const AppContaioner = styled.div`
   display: flex;
@@ -25,24 +24,9 @@ const MainContentContaioner = styled.div`
   padding: 80px 0 80px 0;
 `;
 
-async function requestPermissionAndGetToken() {
-  const permission = await Notification.requestPermission();
-  if (permission === "granted") {
-    console.log("알림 권한이 허용됨");
-    try {
-      await registerServiceWorker();
-      await GetFCMToken();
-    } catch (error) {
-      console.error("토큰을 가져오는 도중 오류가 발생했습니다:", error);
-    }
-  } else {
-    console.log("알림 권한 허용 안됨");
-  }
-}
-
 export default function EventPage() {
   useEffect(() => {
-    requestPermissionAndGetToken();
+    GetUserPermission();
   }, []);
 
   return (
