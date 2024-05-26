02f2310e01a19f983e15c67708f2528ebe95c7ec
석찬 윤 <ysc0731@ajou.ac.kr>
2024-05-26 22:49:45 +0900

    Add GA

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import MyPage from "./pages/MyPage";
 import SavedEventPage from "./pages/SavedEventPage";
 import EventUpload from "./events/EventUpload";
 
+//Google
+import ReactGA from "react-ga";
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
+
 const ROUTER = createBrowserRouter([
   {
     path: "/",
