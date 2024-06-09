aa7e6a8a6f6a539c2fe688e5db8a93d8deb1a862
석찬 윤 <ysc0731@ajou.ac.kr>
2024-06-10 01:47:11 +0900

    UI/UX: Alert box

--- src/fcm/GetUserPermission.jsx
+++ src/fcm/GetUserPermission.jsx
@@ -1,5 +1,18 @@
 import GetFCMToken from "./GetFCMToken";
 import { registerServiceWorker } from "../serviceWorkerRegistration";
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "center-center",
+  showConfirmButton: false,
+  timer: 1000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
 
 const GetUserPermission = async (setIsLoading) => {
   try {
@@ -12,7 +25,7 @@ const GetUserPermission = async (setIsLoading) => {
 
     if (!("Notification" in window)) {
       alert(
-        "브라우저가 웹 알림을 지원하지 않음. 알림기능 사용을 원하시면 브라우저 및 소프트웨어를 업데이트해주세요."
+        "브라우저가 웹 알림을 지원하지 않습니다. 브라우저 설정에서 알림 권한과 소프트웨어 버전을 다시 확인 해 주세요."
       );
       return;
     }
@@ -31,6 +44,10 @@ const GetUserPermission = async (setIsLoading) => {
         await GetFCMToken();
         setIsLoading(false);
       }
+      Toast.fire({
+        icon: "success",
+        title: `알림 설정 요청 성공`,
+      });
     } else {
       alert("알림권한이 허용되어 있지않습니다. 권한을 허용해 주십시오.");
       console.log(
@@ -38,7 +55,10 @@ const GetUserPermission = async (setIsLoading) => {
       );
     }
   } catch (error) {
-    alert("알림 기기 등록 오류");
+    Toast.fire({
+      icon: "error",
+      title: `알림 설정 요청 실패`,
+    });
     console.error("Failed to check or request notification permission:", error);
     setIsLoading(false);
   }
