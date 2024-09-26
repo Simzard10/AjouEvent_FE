b48cda8b4c1a4510b372b02c030f4d9982063496
심재엽 <jysim0326@ajou.ac.kr>
2024-09-27 02:53:13 +0900

    refactor: 삭제(자체 회원가입, 비밀번호 찾기, 비밀번호 변경, 비밀번호 재확인), 추가(구글 로그인, 학과 등록)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./loginPage/LoginPage";
 import SearchEventPage from "./searchPage/SearchEventPage";
-import SignUpPage from "./pages/SignUpPage";
 import SubscribePage from "./subscribePage/SubscribePage";
 import KeywordSubscribePage from "./subscribePage/KeywordSubscribePage";
 import EventDetailPage from "./pages/EventDetailPage";
@@ -13,12 +12,11 @@ import HomePage from "./homePage/HomePage";
 import GuidePage from "./pages/GuidePage";
 import RouteChangeTracker from "./RouteChangeTracker";
 import PrivacyAgreementPage from "./pages/PrivacyAgreementPage";
-import FindPasswordPage from "./pages/FindPasswordPage";
 import { Analytics } from '@vercel/analytics/react';
-import ChangePasswordPage from "./pages/ChangePasswordPage";
-import PasswordConfirmationPage from "./pages/PasswordConfirmationPage";
 import ProfileModificationPage from "./pages/ProfileModificationPage";
 import DeleteAccountPage from "./pages/DeleteAccountPage";
+import RegisterMebmerInfoPage from "./pages/RegisterMebmerInfoPage";
+import SignUpSelectPage from "./pages/SignUpSelectPage";
 
 const ROUTER = createBrowserRouter([
   {
@@ -40,10 +38,6 @@ const ROUTER = createBrowserRouter([
   {
     path: "/privacy-agreement",
     element: <PrivacyAgreementPage />,
-  },
-  {
-    path: "/signUp",
-    element: <SignUpPage />,
   },
   {
     path: "/mypage",
@@ -70,18 +64,6 @@ const ROUTER = createBrowserRouter([
     element: <GuidePage />,
   },
   {
-    path: "/findPassword",
-    element: <FindPasswordPage />,
-  },
-  {
-    path: "/change-password",
-    element: <ChangePasswordPage />,
-  },
-  {
-    path: "/password-confirmation",
-    element: <PasswordConfirmationPage />,
-  },
-  {
     path: "/profile-modification",
     element: <ProfileModificationPage />,
   },
@@ -89,6 +71,14 @@ const ROUTER = createBrowserRouter([
     path: "/delete-account",
     element: <DeleteAccountPage />,
   },
+  {
+    path: "/register-info",
+    element: <RegisterMebmerInfoPage />,
+  },
+  {
+    path: "/signUp/select",
+    element: <SignUpSelectPage />,
+  }
 ]);
 
 function App() {
